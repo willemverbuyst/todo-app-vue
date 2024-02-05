cd0d9177d9f6c6dc4ab4082452699d842ef2a8b8
Willem Verbuyst <willemverbuyst@gmail.com>
2024-02-05 19:22:32 +0100

    Create title component, pass msg

---
+++ src/components/TitleComponent.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div class="title">
+    <h1 class="green">{{ msg }}</h1>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+}
+
+.title h1 {
+  text-align: center;
+}
+</style>
